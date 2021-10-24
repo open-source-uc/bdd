dce1c29a2636341f9cbf4ec5042991db66fb5e33
Benjamín Vicente <benjavicente@uc.cl>
2021-10-24 19:53:49 -0300

    feat: psql database

---
+++ readme.md
@@ -0,0 +1,11 @@
+# BDDUC
+
+## SetUp
+
+### Python
+
+Se necesita tener instalado `poetry`.
+
+### BDD
+
+Se necesita tener instalado `postgresql`.
--- src/db/schema.py
+++ src/db/schema.py
@@ -1,8 +1,11 @@
-from sqlmodel import Field, SQLModel, Relationship
+from sqlalchemy.orm import relation
+from sqlmodel import Field, SQLModel, Relationship, Session
 
 # sqlalchemy debería ser evitado, pero la API de sqlmodel no es tan completa aún
 from sqlalchemy.sql.sqltypes import Enum
 from sqlalchemy import Column
+# from geoalchemy2 import Geometry
+
 
 from typing import Optional, List
 import enum
@@ -31,7 +34,8 @@ class Subject(SQLModel, table=True):
     name: str
     credits: int
     initials: str
-    school_id: Optional[int] = Field(default=None, foreign_key="school.id", primary_key=True)
+    courses: List["Course"] = Relationship(back_populates="subject")
+    school_id: Optional[int] = Field(default=None, foreign_key="school.id")
     school: Optional["School"] = Relationship(back_populates="subjects")
     syllabus: str
     academic_level: str
@@ -42,9 +46,9 @@ class Subject(SQLModel, table=True):
         sa_column=Column(Enum(RequirementRelationEnum))
     )
     # _prerequisites: list["PrerequisitesOrGroup"] = Relationship(back_populates="subjects")
-    equivalences: List["Subject"] = Relationship(
-        back_populates="equivalences", link_model=SubjectEquivalencies
-    )
+    # equivalences: List["Subject"] = Relationship(
+    #     back_populates="equivalences", link_model=SubjectEquivalencies,
+    # )
 
     # @property
     # def prerequisites(self):
@@ -54,7 +58,7 @@ class Subject(SQLModel, table=True):
 class PrerequisitesOrGroup(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     subject_id: int = Field(foreign_key="subject.id", primary_key=True)
-    subject: Subject = Relationship(back_populates="_prerequisites")
+    # subject: Subject = Relationship(back_populates="_prerequisites")
 
 
 class PrerequisitesAndGroup(SQLModel, table=True):
@@ -65,7 +69,7 @@ class PrerequisitesAndGroup(SQLModel, table=True):
 
 class RestrictionsOrGroup(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    subject_id: int = Field(foreign_key="subject.id", primary_key=True)
+    subject_id: int = Field(foreign_key="subject.id")
 
 
 class RestrictionsAndGroup(SQLModel, table=True):
@@ -78,14 +82,17 @@ PeriodEnum = enum.Enum("PeriodEnum", ["S1", "S2", "TAV"])
 
 class Course(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    subject: Subject = Relationship()
+    subject_id: int = Field(foreign_key="subject.id")
+    subject: Subject = Relationship(back_populates="courses")
     year: int
     period: PeriodEnum = Field(sa_column=Column(Enum(PeriodEnum)))
     section: int
     nrc: str
     schedule_summary: str
     campus_id: int = Field(foreign_key="campus.id")
-    campus: "Campus" = Relationship(back_populates="courses")
+    campus: "Campus" = Relationship()
+    faculty_id: int = Field(foreign_key="faculty.id")
+    faculty: "Faculty" = Relationship()
     format: str
     category: str
     fg_area: str
@@ -100,6 +107,8 @@ class Course(SQLModel, table=True):
 class Campus(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
+    faculties: List["Faculty"] = Relationship()
+    places: List["Place"] = Relationship()
 
 
 DayEnum = enum.Enum("DayEnum", ["L", "M", "W", "J", "V", "S"])
@@ -127,6 +136,7 @@ class School(SQLModel, table=True):
     name: str
     website: Optional[str] = None
     description: Optional[str] = None
+    subjects: List[Subject] = Relationship(back_populates="school")
 
 
 class Faculty(SQLModel, table=True):
@@ -139,31 +149,34 @@ class Faculty(SQLModel, table=True):
 
 
 class CategoryOfPlace(SQLModel, table=True):
-    course_id: Optional[int] = Field(default=None, foreign_key="course.id", primary_key=True)
+    category_id: Optional[int] = Field(default=None, foreign_key="placecategory.id", primary_key=True)
     place_id: Optional[int] = Field(default=None, foreign_key="place.id", primary_key=True)
 
 
 class Place(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    #   latLng: point
-    #   polygon: polygon?
+    lat: float
+    lng: float
+    # latLng: list = Field(sa_column=Column(Geometry("POINT")))
+    # polygon: Optional[list] = Field(default=None, sa_column=Column(Geometry("POLYGON")))
     campus_id: int = Field(default=None, foreign_key="campus.id")
     campus: Campus = Relationship(back_populates="places")
     name: str
-    floor: int
+    floor: Optional[int] = None
     notes: Optional[str] = None
-    description: str
+    description: Optional[str] = None
     categories: List["PlaceCategory"] = Relationship(
-        back_populates="places", link_model=CategoryOfPlace
+        back_populates="places", link_model=CategoryOfPlace,
     )
-    parent_id: Optional[int]
+    parent_id: Optional[int] = Field(default=None, foreign_key="place.id")
     parent: "Place" = Relationship(back_populates="child")
+    child: "Place" = Relationship()
 
 
 class PlaceCategory(SQLModel, table=True):
     name: str
     id: Optional[int] = Field(default=None, primary_key=True)
-    places: list[Place] = Relationship(back_populates="categories")
+    places: list[Place] = Relationship(back_populates="categories", link_model=CategoryOfPlace)
 
 
 class UniversityEvents(SQLModel, table=True):
@@ -178,6 +191,9 @@ class UniversityEvents(SQLModel, table=True):
 
 if __name__ == "__main__":
     from sqlmodel import create_engine, SQLModel
-
-    engine = create_engine("sqlite://", echo=True)  # in memory temp DB
+    # engine = create_engine("sqlite://", echo=True)  # in memory temp DB
+    engine = create_engine("postgresql://benjavicente:benjavicente@localhost/bdduc", echo=True)
     SQLModel.metadata.create_all(engine)
+    session = Session(engine)
+    # session.add(Place(lat=1, lng=3, name="lugar x"))
+    # session.commit()
