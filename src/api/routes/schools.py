1042e970043b6a222ea0faab5e5652a117f02135
Nicolás Mc <nicomcintyrew@gmail.com>
2022-07-10 22:52:43 -0400

    feat: add endpoint to get single school

--- src/api/routes/schools.py
+++ src/api/routes/schools.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi_pagination import Page, add_pagination
 from fastapi_pagination.ext.sqlmodel import paginate
 from sqlmodel import Session, select
@@ -12,6 +12,14 @@ school_router = APIRouter()
 @school_router.get("/", response_model=Page[School])
 def get_schools(db: Session = Depends(get_db)):
     return paginate(db, select(School))
+
+@school_router.get("/{id}/")
+def get_school(id: int, db: Session = Depends(get_db)) -> School:
+    school = db.exec(select(School).where(School.id == id)).one_or_none()
+    if school is None:
+        raise HTTPException(404)
+
+    return school
 
 
 @school_router.get("/{school_id}/subjects/", response_model=Page[Subject])
