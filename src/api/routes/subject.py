03788dfe66e8cbd59871e389d300ea6f5898a15f
Nicolás Mc <nicomcintyrew@gmail.com>
2022-07-14 22:29:14 -0400

    remove obsolete and redundant endpoints

--- src/api/routes/subject.py
+++ src/api/routes/subject.py
@@ -1,11 +1,9 @@
-from typing import List
-
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi_pagination import Page, add_pagination
 from fastapi_pagination.ext.sqlmodel import paginate
 from sqlmodel import Session, select
 
-from ...db import Course, PeriodEnum, Subject, Term
+from ...db import Course, Subject, Term
 from ..models import SubjectResponse, SubjectFullResponse, CourseResponse
 from ..utils import get_db
 
@@ -41,47 +39,6 @@ def get_subject_sections(
             query = query.where(Term.period == period)
 
     return paginate(db, query)
-
-
-@subject_router.get("/{id}/requirements/", response_model=list[list[Subject]])
-def get_subject_requirements(id: int, db: Session = Depends(get_db)):
-    s = db.exec(select(Subject).where(Subject.id == id)).one_or_none()
-    if s is None:
-        raise HTTPException(404)
-
-    return s.get_prerequisites()
-
-
-@subject_router.get("/{id}/sections/")
-def get_all_course_sections(id: int, db: Session = Depends(get_db)) -> List[Course]:
-    subject = db.exec(select(Subject).where(Subject.id == id)).one_or_none()
-    if subject is None:
-        raise HTTPException(404)
-
-    return db.exec(select(Course).where(Course.subject_id == id)).all()
-
-
-@subject_router.get("/{id}/{year}/{period}/")
-def get_all_course_sections_by_academic_period(
-    id: int,
-    year: int,
-    period: PeriodEnum,
-    db: Session = Depends(get_db),
-):
-    subject = db.exec(select(Subject).where(Subject.id == id)).one_or_none()
-    if subject is None:
-        raise HTTPException(404)
-
-    # TODO: inlcude: { model: Course, where: course.randido_en = periodo academico }
-    return db.exec(
-        select(Course, Subject, Term).where(
-            Course.subject_id == id,
-            Term.year == year,
-            Term.period == period,
-            Course.subject_id == Subject.id,
-            Term.id == Course.term_id,
-        )
-    ).all()
 
 
 add_pagination(subject_router)
